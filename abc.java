c797fb4dad2e69cbca3913b07d904cea75bc36cd
Ajay <ajaykumar.medure1234@gmail.com>
2022-03-24 16:20:15 +0530

    JENKINS

---
+++ abc.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+class CheckEvenOdd
+{
+  public static void main(String args[])
+  {
+    int num;
+    System.out.println("Enter an Integer number:");
+
+    //The input provided by user is stored in num
+    Scanner input = new Scanner(System.in);
+    num = input.nextInt();
+
+    /* If number is divisible by 2 then it's an even number
+     * else odd number*/
+    if ( num % 2 == 0 )
+
+<<<<<<< HEAD
+int num;
+    System.out.println("Enter an Integer number:");
+
+    //The input provided by user is stored in num
+    Scanner input = new Scanner(System.in);
+    num = input.nextInt();
+
+    /* If number is divisible by 2 then it's an even number
+     * else odd number*/
+    if ( num % 2 == 0 )
+        System.out.println("Entered number is even");
+     else
+        System.out.println("Entered number is odd");
+  }
+   if ( num % 2 == 0 )
+        System.out.println("Entered number is even");
+     else
+        System.out.println("Entered number is odd");
+}
+if ( num % 2 == 0 )
+        System.out.println("Entered number is even");
+     else
+        System.out.println("Entered number is odd");
+}
+Srireddy Comme3nts
+
+Don't use my name in your code siva
+
+Don't use my name in your code \---------------------------------------------------====
+=======
+>>>>>>> 3308c4b8ee7d91579bb064cdd51f9837bb89d65a
